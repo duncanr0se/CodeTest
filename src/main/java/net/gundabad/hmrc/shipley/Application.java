6c91c1be1e45fc9f210e771b22892d82d395172f
Duncan Rose <drose@gundabad.net>
2017-04-25 16:06:52 +0100

    Add Application class

---
+++ src/main/java/net/gundabad/hmrc/shipley/Application.java
@@ -0,0 +1,10 @@
+package net.gundabad.hmrc.shipley;
+
+/**
+ * Shopping cart application for pre-interview coding test.
+ *
+ * Assumption: this will be accessed via tests or other code and no command-
+ * line interface is needed
+ */
+public class Application {
+}
