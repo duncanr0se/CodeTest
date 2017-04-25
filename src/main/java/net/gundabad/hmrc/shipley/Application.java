917bf08da7db7c3abe394e02e3d407bd33cce191
Duncan Rose <drose@gundabad.net>
2017-04-25 16:52:05 +0100

    Stage 1 first-pass done, tests passing.
    Thinking about refactoring "itemCost" method, it's a bit messy.

--- src/main/java/net/gundabad/hmrc/shipley/Application.java
+++ src/main/java/net/gundabad/hmrc/shipley/Application.java
@@ -1,10 +1,64 @@
 package net.gundabad.hmrc.shipley;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
  * Shopping cart application for pre-interview coding test.
  *
  * Assumption: this will be accessed via tests or other code and no command-
  * line interface is needed
  */
-public class Application {
+class Application {
+
+    private Application() {
+        throw new IllegalAccessError("Utility class");
+    }
+
+    /**
+     * Given the name of an item, return the cost in pence of that item.
+     * Throws a {@code RuntimeException} if the item is not recognised.
+     *
+     * @param item The name of an item; one of {@code "apple"} or {@code "orange"}
+     * @return The cost in pence of the item
+     * @throws RuntimeException if the item is not known (i.e., is not
+     * either "apple" or "orange")
+     */
+    private static long itemCost(String item) {
+        // options:
+        // 1) could use list of enum (name, price)
+        // 2) could use list of [name_1, price_1, ..., name_n, price_n]
+        // 3) could use map of name : price
+        switch(item) {
+            case "apple":
+                return 60L;
+            case "orange":
+                return 25L;
+            default:
+                throw new RuntimeException("Unrecognised item in cart: " + item);
+        }
+    }
+
+    /**
+     * Given a list containing the names of known items, return the total cost
+     * of those items.
+     * Throws a {@code RuntimeException} if any of the items are not recognised.
+     *
+     * @param shoppingCart The names of the items to total; each element must
+     *                     be one of {@code "apple"} or {@code "orange"}.
+     * @return The cost of the items with the fractional part being the pence
+     * and the integer part being the pounds.
+     * @throws RuntimeException if any item is not known (i.e., is not
+     * either "apple" or "orange")
+     */
+    public static BigDecimal getCartTotalCost(List<String> shoppingCart) {
+        long costAsLong = shoppingCart.stream()
+                .mapToLong(Application::itemCost)
+                .sum();
+
+        // Assumption: this method returns pounds + pence. Could be a SRP
+        // violation, perhaps this should return total in pence and that
+        // should be formatted elsewhere.
+        return BigDecimal.valueOf(costAsLong).divide(BigDecimal.valueOf(100)).setScale(2);
+    }
 }
--- src/test/java/net/gundabad/hmrc/shipley/ApplicationTest.java
+++ src/test/java/net/gundabad/hmrc/shipley/ApplicationTest.java
@@ -2,7 +2,15 @@ package net.gundabad.hmrc.shipley;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 /**
  * Test application functionality
@@ -18,16 +26,54 @@ public class ApplicationTest {
         } catch (ClassNotFoundException e) {
             // ignore exception
         }
+
         assertThat(applicationClass).isNotNull();
     }
 
-    // method to return price for named item; note that this does not need to be exposed...
-    // apple => 60p
-    // orange => 25p
-    // check function takes list of names of items, returns total cost for items
-    // given single apple its cost is 60p
-    // given single orange its cost is 25p
-    // given any other name, its cost is 0p
-    // given [apple, apple, orange, apple] => 2.05
-    // assume cost needs to be formatted as a price
+    @Test
+    public void given_shopping_cart_list_when_single_apple_only_then_total_cost_is_60p() {
+        List<String> shoppingCart = Collections.singletonList("apple");
+
+        BigDecimal cost = Application.getCartTotalCost(shoppingCart);
+
+        assertThat(cost).isEqualTo(new BigDecimal("0.60"));
+    }
+
+    @Test
+    public void given_shopping_cart_list_when_single_orange_only_then_total_cost_is_25p() {
+        List<String> shoppingCart = Collections.singletonList("orange");
+
+        BigDecimal cost = Application.getCartTotalCost(shoppingCart);
+
+        assertThat(cost).isEqualTo(new BigDecimal("0.25"));
+    }
+
+    @Test
+    public void given_shopping_cart_list_when_unknown_item_only_then_exception_thrown() {
+        List<String> shoppingCart = Collections.singletonList("appl");
+
+        Throwable thrown = catchThrowable(() -> Application.getCartTotalCost(shoppingCart));
+
+        assertThat(thrown).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void given_shopping_cart_list_when_contains_unknown_item_then_exception_thrown() {
+        List<String> shoppingCart = Arrays.asList("apple", "apple", "orangutan", "apple");
+
+        Throwable thrown = catchThrowable(() -> Application.getCartTotalCost(shoppingCart));
+
+        assertThat(thrown).isInstanceOf(RuntimeException.class);
+
+    }
+
+    @Test
+    public void given_shopping_cart_list_when_contains_known_items_then_expected_total_returned() {
+        List<String> shoppingCart = Arrays.asList("apple", "apple", "orange", "apple");
+
+        BigDecimal cost = Application.getCartTotalCost(shoppingCart);
+
+        assertThat(cost).isEqualTo(new BigDecimal("2.05"));
+
+    }
 }
